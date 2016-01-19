53ccffaac8d7856ab377715ac4af3159fdb8e119
mateusz <mateusz@mateusz-Vostro-3555>
2016-01-19 11:58:16 +0100

    Defined custom functions for writing in hard faults.

--- core/panic.c
+++ core/panic.c
@@ -23,6 +23,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "assert.h"
 #include "cpu.h"
@@ -30,6 +31,31 @@
 #include "lpm.h"
 #include "panic.h"
 #include "arch/panic_arch.h"
+#include "periph/hard_communication.h"
+
+void h_puts(const char* msg)
+{
+#ifdef STDIO_HARD
+    hard_com_put(msg, strlen(msg));
+    hard_com_put("\n", strlen(msg));
+#else
+    puts(msg);
+#endif
+}
+
+void h_printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+#ifdef STDIO_HARD
+    char tmp[STDIO_HARD_LINELENGTH];
+    vsnprintf(tmp, STDIO_HARD_LINELENGTH, format, args);
+    hard_com_put(tmp, STDIO_HARD_LINELENGTH);
+#else
+    vprintf (format, args);
+#endif
+    va_end(args);
+}
 
 #if defined(DEVELHELP) && defined(MODULE_PS)
 #include "ps.h"
